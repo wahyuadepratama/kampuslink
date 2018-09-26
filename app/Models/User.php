1490ae2585c404f6dbe348fc4200a4f40d058f47
wahyuadepratama <1511521024@student.unand.ac.id>
2018-09-26 22:04:13 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Models;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'name', 'username' ,'email','role_id','password','avatar'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token'
+    ];
+
+    public function role(){
+      return $this->belongsTo('App\Models\Role','role_id');
+    }
+}
